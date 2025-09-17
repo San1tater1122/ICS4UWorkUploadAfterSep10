b6cc451d49a80bf2f5417b60e981454dc71a49e3
San1tater <98987733+San1tater1122@users.noreply.github.com>
2025-09-16 22:35:49 -0400

    Delete Sep16 directory

--- Sep16/AdLoopQ1.java
+++
@@ -1,9 +0,0 @@
-package Sep16;
-
-public class AdLoopQ1 {
-    public static void main(String[] args){
-        for (byte i = 1; i <= 50; i++){ // start with 1, until 50
-            System.out.println(i);
-        }
-    }
-}
